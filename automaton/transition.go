f90686f22b12f86226f9eb9ee1fb4588205b51d4
ikawaha <ikawaha@users.noreply.github.com>
2022-08-07 08:38:29 +0900

    Fix bug

--- automaton/transition.go
+++ automaton/transition.go
@@ -11,12 +11,12 @@ var transition = map[pair]state{
 	{state: s2, input: "スコ"}: s3,
 	{state: s3, input: "スコ"}: s4,
 
-	{state: s4, input: "ドド"}: s5, {state: s5, input: "スコ"}: s0,
+	{state: s4, input: "ドド"}: s5, {state: s4, input: "スコ"}: s0,
 	{state: s5, input: "スコ"}: s6,
 	{state: s6, input: "スコ"}: s7,
 	{state: s7, input: "スコ"}: s8,
 
-	{state: s8, input: "ドド"}: s9, {state: s9, input: "スコ"}: s0,
+	{state: s8, input: "ドド"}: s9, {state: s8, input: "スコ"}: s0,
 	{state: s9, input: "スコ"}:  s10,
 	{state: s10, input: "スコ"}: s11,
 	{state: s11, input: "スコ"}: s12,
